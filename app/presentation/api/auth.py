3228b0b707e4edc4ab50c8ad1bb4858bc7a71c70
podz <podzolkovn@gmail.com>
2025-01-16 01:26:54 +0500

    [feature/bearer-login] added dependeny for tokens

--- app/presentation/api/auth.py
+++ app/presentation/api/auth.py
@@ -1,12 +1,17 @@
 from app.core.security import authentication_backend
 from app.domain.schemas.user import UserRead, UserCreate
 from app.presentation.api.fastapi_users import fastapi_users
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
+http_bearer: HTTPBearer = HTTPBearer(auto_error=False)
 
 router: APIRouter = APIRouter(
     prefix="/auth",
     tags=["Auth"],
+    dependencies=[
+        Depends(http_bearer),
+    ],
 )
 
 router.include_router(
