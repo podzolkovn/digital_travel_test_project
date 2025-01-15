c8ceec33780000cff98395e231e09d3c20c6ab94
podz <podzolkovn@gmail.com>
2025-01-16 01:15:19 +0500

    [feature/register-user] added endpoint for register user

--- app/presentation/api/auth.py
+++ app/presentation/api/auth.py
@@ -1,4 +1,5 @@
 from app.core.security import authentication_backend
+from app.domain.schemas.user import UserRead, UserCreate
 from app.presentation.api.fastapi_users import fastapi_users
 from fastapi import APIRouter
 
@@ -10,4 +11,12 @@ router: APIRouter = APIRouter(
 
 router.include_router(
     router=fastapi_users.get_auth_router(authentication_backend),
+)
+
+
+router.include_router(
+    fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
 )
