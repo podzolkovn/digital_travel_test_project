c2cc2c693c9df31f9e5cbcb656a3fbd9352eb601
podz <podzolkovn@gmail.com>
2025-01-16 00:39:07 +0500

    [feature/user-manager] created user manager

---
+++ app/application/managers/user.py
@@ -0,0 +1,49 @@
+from logging.config import dictConfig
+from typing import Optional
+import logging
+from app.core.config import settings
+from app.core.logger import LoggerConfig
+from app.domain.models.auth import User
+from fastapi import Request
+from fastapi_users import BaseUserManager, IntegerIDMixin
+
+dictConfig(LoggerConfig().model_dump())
+logger = logging.getLogger("ai_prompt")
+
+SECRET = settings.SECRET_JWT
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(
+        self,
+        user: User,
+        request: Optional[Request] = None,
+    ):
+        logger.info(
+            "User %r has registered.",
+            user.id,
+        )
+
+    # async def on_after_forgot_password(
+    #     self,
+    #     user: User,
+    #     token: str,
+    #     request: Optional[Request] = None,
+    # ):
+    #     logger.info(
+    #         "User %r has forgot their password. Reset token: %r",
+    #         user.id,
+    #         token,
+    #     )
+    #
+    # async def on_after_request_verify(
+    #     self, user: User, token: str, request: Optional[Request] = None
+    # ):
+    #     logger.info(
+    #         "Verification requested for user %r. Verification token: %r",
+    #         user.id,
+    #         token,
+    #     )
--- app/domain/dependencies/user.py
+++ app/domain/dependencies/user.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 
+from app.application.managers.user import UserManager
 from app.domain.models.auth import User
 from app.infrastructure.db import get_async_session
 from fastapi import Depends
@@ -8,7 +9,13 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_user_db(session: "AsyncSession" = Depends(get_async_session)):
+async def get_user_db(
+    session: "AsyncSession" = Depends(get_async_session),
+):
     yield User.get_db(session=session)
 
 
+async def get_user_manager(
+    user_db=Depends(get_user_db),
+):
+    yield UserManager(user_db)
