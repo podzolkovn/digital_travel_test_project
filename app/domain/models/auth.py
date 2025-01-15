abe203c26279576dde3dfe099788f006e1804486
podz <podzolkovn@gmail.com>
2025-01-15 23:31:56 +0500

    [feature/add-method] added method for user db

--- app/domain/models/auth.py
+++ app/domain/models/auth.py
@@ -1,5 +1,10 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from app.domain.models.abstract import AbstractModel
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(AbstractModel, SQLAlchemyBaseUserTable[int]):
@@ -7,3 +12,8 @@ class User(AbstractModel, SQLAlchemyBaseUserTable[int]):
     @property
     def role(self) -> str:
         return "admin" if self.is_superuser else "user"
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
+    
