6c1a03ba31f377a0542fd94c0cd4341693a04e06
podz <podzolkovn@gmail.com>
2025-01-16 15:09:49 +0500

    [feature/endpoints] added template for orders endpoints

--- app/presentation/api/order.py
+++ app/presentation/api/order.py
@@ -1,8 +1,9 @@
+from starlette import status
+
 from app.domain.models.auth import User
-from app.domain.schemas.order import OrderWrite
-from app.domain.schemas.user import UserRead
+from app.domain.schemas.order import OrderWrite, OrderRead, OrderUpdate
 from app.presentation.api.fastapi_users import current_user
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 
 router: APIRouter = APIRouter(
     prefix="/orders",
@@ -10,9 +11,77 @@ router: APIRouter = APIRouter(
 )
 
 
-@router.post("")
-async def get_orders(
+@router.post(
+    path="",
+    response_model=OrderRead,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_orders(
     data: OrderWrite,
     user: User = Depends(current_user),
-):
-    return {"message": "Hello World", "user": UserRead.model_validate(user)}
+) -> Response:
+    return Response(
+        content="Hello World",
+        media_type="application/json",
+        status_code=status.HTTP_201_CREATED,
+    )
+
+
+@router.get(
+    path="",
+    response_model=OrderRead,
+    status_code=status.HTTP_200_OK,
+)
+async def get_orders(user: User = Depends(current_user)) -> Response:
+    return Response(
+        content="Hello World",
+        media_type="application",
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.get(
+    "/{order_id}",
+    response_model=OrderRead,
+    status_code=status.HTTP_200_OK,
+)
+async def get_order(
+    order_id: int,
+    user: User = Depends(current_user),
+) -> Response:
+    return Response(
+        content="Hello World",
+        media_type="application",
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.patch(
+    "/{order_id}",
+    response_model=OrderUpdate,
+    status_code=status.HTTP_200_OK,
+)
+async def update_order(
+    order_id: int,
+    user: User = Depends(current_user),
+) -> Response:
+    return Response(
+        content="Hello World",
+        media_type="application",
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.delete(
+    "/{order_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_order(
+    order_id: int,
+    user: User = Depends(current_user),
+) -> Response:
+    return Response(
+        content="Hello World",
+        media_type="application",
+        status_code=status.HTTP_204_NO_CONTENT,
+    )
