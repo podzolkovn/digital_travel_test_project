f473b445ad51554ddf325624da48fc4d9a5b533a
podz <podzolkovn@gmail.com>
2025-01-17 14:17:44 +0500

    [feature/validators] added validator for check user, check nullable data

--- app/application/managers/user.py
+++ app/application/managers/user.py
@@ -1,11 +1,14 @@
 from logging.config import dictConfig
 from typing import Optional
 import logging
+
+from starlette.status import HTTP_400_BAD_REQUEST
+
 from app.core.config import settings
 from app.core.logger import LoggerConfig
 from app.domain.models.auth import User
-from fastapi import Request
-from fastapi_users import BaseUserManager, IntegerIDMixin
+from fastapi import Request, HTTPException
+from fastapi_users import BaseUserManager, IntegerIDMixin, models
 
 dictConfig(LoggerConfig().model_dump())
 logger: logging = logging.getLogger("digital_travel_concierge")
@@ -20,6 +23,24 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
     reset_password_token_secret: str = SECRET
     verification_token_secret: str = SECRET
+
+    async def get(self, id: models.ID) -> models.UP:
+        """
+        Get a user by id.
+
+        :param id: Id. of the user to retrieve.
+        :raises UserNotExists: The user does not exist.
+        :return: A user.
+        """
+        user = await self.user_db.get(id)
+
+        if user is None:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,
+                detail={"user_id": f"User {id} does not exist."},
+            )
+
+        return user
 
     async def on_after_register(
         self,
